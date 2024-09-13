ee349c39631f6bfbe6adefd8b494a4c0083e4d11
Hassnain sher <hassnainshair55@gmail.com>
2024-09-13 19:28:14 +0500

    CPP first day data types

---
+++ c++/test/test/test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//int main()
+//{
+//	int a;
+//	a = 12;
+//	cout << "size of int" << sizeof(a) << endl;
+//	float b;
+//	cout << "size of b"<<sizeof(b) << endl;
+//}
+
+//int main() {
+//	int a1;
+//	cin >> a1;
+//	cout << a1;
+//}
+
+
